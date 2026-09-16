6dae1b872f149aed0972e375de148285dbeba193
syndicate604 <syndicate604@users.noreply.github.com>
2026-09-16 02:25:24 +0000

    deploy: 7dc02b82e1b7e4818ca3df5ba44e48d0a11de175

---
+++ trait.impl/std/io/trait.Write.js
@@ -0,0 +1,9 @@
+(function() {
+    const implementors = Object.fromEntries([["ruv_fann",[["impl&lt;W: <a class=\"trait\" href=\"https://doc.rust-lang.org/1.98.1/std/io/trait.Write.html\" title=\"trait std::io::Write\">Write</a>&gt; <a class=\"trait\" href=\"https://doc.rust-lang.org/1.98.1/std/io/trait.Write.html\" title=\"trait std::io::Write\">Write</a> for <a class=\"struct\" href=\"ruv_fann/io/compression/struct.CompressedWriter.html\" title=\"struct ruv_fann::io::compression::CompressedWriter\">CompressedWriter</a>&lt;W&gt;",0]]]]);
+    if (window.register_implementors) {
+        window.register_implementors(implementors);
+    } else {
+        window.pending_implementors = implementors;
+    }
+})()
+//{"start":59,"fragment_lengths":[464]}
